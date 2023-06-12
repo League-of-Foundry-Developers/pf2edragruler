39c5c96c08da850a437dff09d102fdf8c2c05a00
Avery <74130268+velara@users.noreply.github.com>
2023-06-12 13:46:16 -0600

    Update settings.js
    Add localization to module settings

--- js/settings.js
+++ js/settings.js
@@ -1,32 +1,32 @@
 export function registerSettings() {
 	// Create the setting for Automatic Movement switching for later use.
-	game.settings.register("pf2e-dragruler", "auto", {
-		name: "Automatic Movement Switching",
-		hint: "If enabled, positive elevations will automatically use fly speed, negative elevations will use burrow, and tokens in water or aquatic terrain will use swim speeds, if an actor does not have that speed, it will instead use their land speed.",
+game.settings.register("pf2e-dragruler", "auto", {
+		name: game.i18n.localize("pf2e-dragruler.settings.auto.name"),
+		hint: game.i18n.localize("pf2e-dragruler.settings.auto.hint"),
 		scope: "world",
 		config: true,
 		type: Boolean,
 		default: false
 }),
 game.settings.register("pf2e-dragruler", "scene", {
-	name: "Scene Environment Automation",
-	hint: "Requires Enhanced Terrain Layer. If enabled, actors in Sky scenes will automatically use fly speed, and those in aquatic terrain will use swim speeds, if an actor does not have a speed, will use their land speed.",
+	name: game.i18n.localize("pf2e-dragruler.settings.scene.name"),
+	hint: game.i18n.localize("pf2e-dragruler.settings.scene.hint"),
 	scope: "world",
 	config: true,
 	type: Boolean,
 	default: false
 }),
 game.settings.register("pf2e-dragruler", "offTurnMovement", {
-	name: "Ignore Off Turn Movement",
-	hint: "Requires movement history to be enabled. Automatically resets movement history at the start of each actor's turn in combat, meaning any movement performed off turn as a reaction won't effect the movement history, on your turn.",
+	name: game.i18n.localize("pf2e-dragruler.settings.offTurnMovement.name"),
+	hint: game.i18n.localize("pf2e-dragruler.settings.offTurnMovement.hint"),
 	scope: "world",
 	config: true,
 	type: Boolean,
 	default: false
 }),
 game.settings.register("pf2e-dragruler", "partialMovements", {
-	name: "Partial Movements",
-	hint: "Treat each completed drag as an action, and reduce viable move distance as appropriate",
+	name: game.i18n.localize("pf2e-dragruler.settings.partialMovements.name"),
+	hint: game.i18n.localize("pf2e-dragruler.settings.partialMovements.hint"),
 	scope: "world",
 	config: true,
 	type: Boolean,
